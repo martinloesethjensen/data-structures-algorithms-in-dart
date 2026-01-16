c52743437a1577285dd3d99dc1f0efdfeab1adb4
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-16 23:29:05 +0100

    completed challenges

---
+++ chapter-6-queues/lib/README.md
@@ -0,0 +1,19 @@
+# Challenges
+
+## Challenge 1
+
+Explain the difference between a stack and a queue. Provide two real-life examples
+for each data structure.
+
+Stack.
+Is like stacking boxes on top of each other. You need to take the top box first if you take a box.
+A real application use of this data structure would be an undo feature like with drawing apps that have an undo button.
+
+Queue.
+FIFO (first in first out). Imagine a literal queue buying tickets for a movie at a theater.
+A real application use of this data structure would be to run operations in a specific order.
+Like handling events in order of execution they come in.
+
+## Challenge 2
+
+See PDF
--- chapter-6-queues/lib/main.dart
+++ chapter-6-queues/lib/main.dart
@@ -1,4 +1,79 @@
 import 'package:chapter_6_queues/queue.dart';
+import 'package:common/common.dart';
+
+extension BoardGameManager<E> on QueueRingBuffer {
+  E? nextPlayer() {
+    final player = dequeue();
+    if (player != null) enqueue(player);
+    return player;
+  }
+}
+
+void challenge3() {
+  final monopolyTurn = QueueRingBuffer<String>(2);
+  monopolyTurn.enqueue('Alice');
+  monopolyTurn.enqueue('Bob');
+  String? player;
+  for (var i = 0; i <= 11; i++) {
+    player = monopolyTurn.nextPlayer();
+  }
+  print('$player wins!');
+}
+
+enum Direction { front, back }
+
+abstract class Deque<E> {
+  bool get isEmpty;
+  E? peek(Direction from);
+  bool enqueue(E element, Direction to);
+  E? dequeue(Direction from);
+}
+
+class DequeList<E> implements Deque<E> {
+  final _list = DoublyLinkedList<E>();
+
+  @override
+  E? dequeue(Direction from) {
+    return switch (from) {
+      Direction.front => _list.pop(),
+      Direction.back => _list.removeLast(),
+    };
+  }
+
+  @override
+  bool enqueue(E element, Direction to) {
+    switch (to) {
+      case Direction.front:
+        _list.push(element);
+      case Direction.back:
+        _list.append(element);
+    }
+    return true;
+  }
+
+  @override
+  bool get isEmpty => _list.isEmpty;
+
+  @override
+  E? peek(Direction from) => switch (from) {
+    Direction.front => _list.head?.value,
+    Direction.back => _list.tail?.value,
+  };
+
+  @override
+  String toString() => _list.toString();
+}
+
+void challenge4() {
+  final queue = DequeList<String>();
+  queue.enqueue('Ray', Direction.front);
+  queue.enqueue('Brian', Direction.front);
+  queue.enqueue('Eric', Direction.back);
+  print(queue); // [Brian, Ray, Eric]
+  queue.dequeue(Direction.front);
+  print(queue); // [Ray, Eric,]
+  print(queue.peek(Direction.front)); // Ray
+}
 
 void main() {
   final queue = QueueList<String>();
@@ -36,4 +111,11 @@ void main() {
   queueStack.dequeue();
   print(queueStack); // [Brian, Eric]
   print(queueStack.peek); // Brian
+
+  // Challenges
+  // See challenge 1 & 2 in the README.md
+
+  challenge3();
+
+  challenge4();
 }
