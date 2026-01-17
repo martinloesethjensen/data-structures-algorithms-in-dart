10cf4bb71f2f476ba387ae18752baf9b4c4d94af
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-17 20:41:16 +0100

    for each depth first traversal

--- chapter-7-trees/lib/main.dart
+++ chapter-7-trees/lib/main.dart
@@ -1,1 +1,35 @@
-void main() {}
+import 'package:chapter_7_trees/tree.dart';
+
+void main() {
+  final tree = makeBeverageTree();
+  tree.forEachDepthFirst((node) => print(node.value));
+}
+
+TreeNode<String> makeBeverageTree() {
+  final tree = TreeNode('beverages');
+  final hot = TreeNode('hot');
+  final cold = TreeNode('cold');
+  final tea = TreeNode('tea');
+  final coffee = TreeNode('coffee');
+  final chocolate = TreeNode('cocoa');
+  final blackTea = TreeNode('black');
+  final greenTea = TreeNode('green');
+  final chaiTea = TreeNode('chai');
+  final soda = TreeNode('soda');
+  final milk = TreeNode('milk');
+  final gingerAle = TreeNode('ginger ale');
+  final bitterLemon = TreeNode('bitter lemon');
+  tree.add(hot);
+  tree.add(cold);
+  hot.add(tea);
+  hot.add(coffee);
+  hot.add(chocolate);
+  cold.add(soda);
+  cold.add(milk);
+  tea.add(blackTea);
+  tea.add(greenTea);
+  tea.add(chaiTea);
+  soda.add(gingerAle);
+  soda.add(bitterLemon);
+  return tree;
+}
