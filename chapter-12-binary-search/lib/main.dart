260c09b3775303b1ce6203b492388c78f9469d91
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-20 22:08:22 +0100

    chapter 12 challenges

--- chapter-12-binary-search/lib/main.dart
+++ chapter-12-binary-search/lib/main.dart
@@ -68,8 +68,43 @@ Range? findRange<E extends Comparable<dynamic>>(
   List<E> list, {
   required E value,
 }) {
-  final start = list.indexOf(value);
-  if (start == -1) return null;
-  final end = list.lastIndexOf(value);
+  if (list.isEmpty) return null;
+  final start = _startIndex(list, value);
+  final end = _endIndex(list, value);
+  if (start == null || end == null) return null;
   return Range(start, end);
+}
+
+int? _startIndex<E extends Comparable<dynamic>>(List<E> list, E value) {
+  if (list[0] == value) return 0;
+  var start = 1;
+  var end = list.length;
+  while (start < end) {
+    var middle = start + (end - start) ~/ 2;
+    if (list[middle] == value && list[middle - 1] != value) {
+      return middle;
+    } else if (list[middle].compareTo(value) < 0) {
+      start = middle + 1;
+    } else {
+      end = middle;
+    }
+  }
+  return null;
+}
+
+int? _endIndex<E extends Comparable<dynamic>>(List<E> list, E value) {
+  if (list[list.length - 1] == value) return list.length;
+  var start = 0;
+  var end = list.length - 1;
+  while (start < end) {
+    var middle = start + (end - start) ~/ 2;
+    if (list[middle] == value && list[middle + 1] != value) {
+      return middle + 1;
+    } else if (list[middle].compareTo(value) > 0) {
+      end = middle;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return null;
 }
