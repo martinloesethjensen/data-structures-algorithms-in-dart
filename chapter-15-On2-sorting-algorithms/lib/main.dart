3f1c0c003ab30d0f11a35781e6333cc3a8c60c97
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-26 21:16:44 +0100

    insertion & selection sort

--- chapter-15-On2-sorting-algorithms/lib/main.dart
+++ chapter-15-On2-sorting-algorithms/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:chapter_15_on2_sorting_algorithms/bubble_sort.dart';
+import 'package:chapter_15_on2_sorting_algorithms/insertion_sort.dart';
+import 'package:chapter_15_on2_sorting_algorithms/selection_sort.dart';
 
 void main() {
-  final list = [9, 4, 10, 3];
+  var list = [9, 4, 10, 3];
   print('Original: $list');
   bubbleSort(list);
   print('Bubble sorted: $list');
+
+  list = [9, 4, 10, 3];
+  print('Original: $list');
+  selectionSort(list);
+  print('Selection sorted: $list');
+
+  list = [9, 4, 10, 3];
+  print('Original: $list');
+  insertionSort(list);
+  print('Insertion sorted: $list');
 }
