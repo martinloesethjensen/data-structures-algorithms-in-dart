a66deb3b9efce955199747e67b504a7adbbbfc57
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-26 15:05:19 +0100

    heap challenges

--- chapter-13-heaps/lib/main.dart
+++ chapter-13-heaps/lib/main.dart
@@ -39,4 +39,57 @@ void main() {
   while (!minHeap.isEmpty) {
     print(minHeap.remove());
   }
+
+  print('--- Challenges ---');
+  challenge1();
+  challenge3();
+  challenge4();
+}
+
+void challenge1() {
+  final integers = [3, 10, 18, 5, 21, 100];
+  print(findNthSmallest(3, integers));
+}
+
+int findNthSmallest(int n, List<int> list) {
+  if (n > list.length) return -1;
+  final heap = Heap<int>(elements: list, priority: Priority.min);
+  return heap.elements[n];
+}
+
+void challenge3() {
+  final heap1 = Heap(elements: [1, 4, 6, 8, 3], priority: Priority.min);
+  final heap2 = Heap(elements: [11, 14, 60, 18, 2]);
+  final combined = combine(heap1, heap2);
+  print(combined);
+}
+
+Heap combine(Heap left, Heap right, [Priority? priority]) {
+  return Heap(
+    elements: List.of([...left.elements, ...right.elements]),
+    priority: priority ?? left.priority,
+  );
+}
+
+void challenge4() {
+  final list = [1, 3, 6, 8, 11];
+  print(list.isMinHeap);
+}
+
+extension ListExt<E extends Comparable<dynamic>> on List<E> {
+  bool get isMinHeap {
+    if (isEmpty) return true;
+    final start = length ~/ 2 - 1;
+    for (var i = start; i >= 0; i--) {
+      final left = 2 * i + 1;
+      final right = 2 * i + 2;
+      if (this[left].compareTo(this[i]) < 0) {
+        return false;
+      }
+      if (right < length && this[right].compareTo(this[i]) < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
