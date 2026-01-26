dcca079dbd8200562acd76e84637daa8f7cd4b9f
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-26 20:48:01 +0100

    bubble sort

---
+++ chapter-15-On2-sorting-algorithms/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:chapter_15_on2_sorting_algorithms/bubble_sort.dart';
+
+void main() {
+  final list = [9, 4, 10, 3];
+  print('Original: $list');
+  bubbleSort(list);
+  print('Bubble sorted: $list');
+}
