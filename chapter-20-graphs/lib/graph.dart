09882386c2f397edbce65e3b3c77c82e5dfba8e3
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-27 22:36:21 +0100

    adjacency list graph

---
+++ chapter-20-graphs/lib/graph.dart
@@ -0,0 +1,85 @@
+class Vertex<T> {
+  const Vertex({required this.index, required this.data});
+  final int index;
+  final T data;
+
+  @override
+  String toString() => data.toString();
+}
+
+class Edge<T> {
+  const Edge(this.source, this.destination, [this.weight]);
+  final Vertex<T> source;
+  final Vertex<T> destination;
+  final double? weight;
+}
+
+enum EdgeType { directed, undirected }
+
+abstract class Graph<E> {
+  Iterable<Vertex<E>> get vertices;
+
+  Vertex<E> createVertex(E data);
+
+  void addEdge(
+    Vertex<E> source,
+    Vertex<E> destination, {
+    EdgeType edgeType,
+    double? weight,
+  });
+
+  List<Edge<E>> edges(Vertex<E> source);
+
+  double? weight(Vertex<E> source, Vertex<E> destination);
+}
+
+class AdjacencyList<E> implements Graph<E> {
+  final Map<Vertex<E>, List<Edge<E>>> _connections = {};
+  var _nextIndex = 0;
+
+  @override
+  void addEdge(
+    Vertex<E> source,
+    Vertex<E> destination, {
+    EdgeType edgeType = EdgeType.undirected,
+    double? weight,
+  }) {
+    _connections[source]?.add(Edge(source, destination, weight));
+    if (edgeType == EdgeType.undirected) {
+      _connections[destination]?.add(Edge(destination, source, weight));
+    }
+  }
+
+  @override
+  Vertex<E> createVertex(E data) {
+    final vertex = Vertex(index: _nextIndex, data: data);
+    _nextIndex++;
+    _connections[vertex] = [];
+    return vertex;
+  }
+
+  @override
+  List<Edge<E>> edges(Vertex<E> source) => _connections[source] ?? [];
+
+  @override
+  Iterable<Vertex<E>> get vertices => _connections.keys;
+
+  @override
+  double? weight(Vertex<E> source, Vertex<E> destination) {
+    final match = edges(
+      source,
+    ).where((edge) => edge.destination == destination);
+    if (match.isEmpty) return null;
+    return match.first.weight;
+  }
+
+  @override
+  String toString() {
+    final result = StringBuffer();
+    _connections.forEach((vertex, edges) {
+      final destinations = edges.map((edge) => edge.destination).join(', ');
+      result.writeln('$vertex --> $destinations');
+    });
+    return result.toString();
+  }
+}
