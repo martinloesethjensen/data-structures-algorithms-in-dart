940fe2a971d5527ab7ceb26c2a4e9fdd8fca058f
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-27 15:17:58 +0100

    merge sort

---
+++ chapter-16-merge-sort/lib/merge_sort.dart
@@ -0,0 +1,80 @@
+List<E> mergeSort<E extends Comparable<dynamic>>(List<E> list) {
+  if (list.length < 2) return list;
+  final middle = list.length ~/ 2;
+  final left = mergeSort(list.sublist(0, middle));
+  final right = mergeSort(list.sublist(middle));
+  return _mergeIterable(left, right);
+}
+
+// ignore: unused_element
+List<E> _merge<E extends Comparable<dynamic>>(List<E> listA, List<E> listB) {
+  var indexA = 0;
+  var indexB = 0;
+  final result = <E>[];
+
+  while (indexA < listA.length && indexB < listB.length) {
+    final valueA = listA[indexA];
+    final valueB = listB[indexB];
+    if (valueA.compareTo(valueB) < 0) {
+      result.add(valueA);
+      indexA += 1;
+    } else if (valueA.compareTo(valueB) > 0) {
+      result.add(valueB);
+      indexB += 1;
+    } else {
+      result.add(valueA);
+      result.add(valueB);
+      indexA += 1;
+      indexB += 1;
+    }
+  }
+  if (indexA < listA.length) {
+    result.addAll(listA.getRange(indexA, listA.length));
+  }
+  if (indexB < listB.length) {
+    result.addAll(listB.getRange(indexB, listB.length));
+  }
+  return result;
+}
+
+List<E> _mergeIterable<E extends Comparable<dynamic>>(
+  Iterable<E> first,
+  Iterable<E> second,
+) {
+  var result = <E>[];
+  var firstIterator = first.iterator;
+  var secondIterator = second.iterator;
+  var firstHasValue = firstIterator.moveNext();
+  var secondHasValue = secondIterator.moveNext();
+
+  while (firstHasValue && secondHasValue) {
+    final firstValue = firstIterator.current;
+    final secondValue = secondIterator.current;
+    if (firstValue.compareTo(secondValue) < 0) {
+      result.add(firstValue);
+      firstHasValue = firstIterator.moveNext();
+    } else if (firstValue.compareTo(secondValue) > 0) {
+      result.add(secondValue);
+      secondHasValue = secondIterator.moveNext();
+    } else {
+      result.add(firstValue);
+      result.add(secondValue);
+      firstHasValue = firstIterator.moveNext();
+      secondHasValue = secondIterator.moveNext();
+    }
+  }
+
+  if (firstHasValue) {
+    do {
+      result.add(firstIterator.current);
+    } while (firstIterator.moveNext());
+  }
+
+  if (secondHasValue) {
+    do {
+      result.add(secondIterator.current);
+    } while (secondIterator.moveNext());
+  }
+
+  return result;
+}
