7532fb6cbe06e32b7cfd0aa5f17edf177b6d0cf2
Martin Jensen <martinloesethjensen@gmail.com>
2026-02-28 20:38:49 +0100

    quicksort recursion impl

--- chapter-19-quicksort/lib/main.dart
+++ chapter-19-quicksort/lib/main.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'quicksort.dart';
+
+void main() {
+  final _defaultList = [8, 2, 10, 0, 9, 18, 9, -1, 5];
+  var list = _defaultList;
+  final sorted = quicksortNaive(list);
+  print(sorted);
+
+  list = _defaultList;
+  quicksortLomuto(list, 0, list.length - 1);
+  print(list);
+
+  list = _defaultList;
+  quicksortHoare(list, 0, list.length - 1);
+  print(list);
+
+  list = _defaultList;
+  quicksortMedian(list, 0, list.length - 1);
+  print(list);
+
+  list = [8, 2, 2, 8, 9, 5, 9, 2, 8];
+  quicksortDutchFlag(list, 0, list.length - 1);
+  print(list);
+}
